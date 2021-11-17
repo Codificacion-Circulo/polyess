940fad6bddd4e388a5a26327ff6d619183f75679
Aditya Jain <deepika@mac.local>
2021-11-17 16:15:50 +0530

    Added a bal function to set balance for state varibale

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,9 +3,13 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract SimpleStorage {
   uint storedData;
+  uint bal;
 
   function set(uint x) public {
     storedData = x;
+  }
+  function setbal(uint _bal) public{
+    bal=_bal;
   }
 
   function get() public view returns (uint) {
