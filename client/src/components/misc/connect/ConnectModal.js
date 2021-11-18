610d75d0afd120a98f1b3d12cf687e2a7f600447
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-19 05:28:06 +0530

    half web3 done

---
+++ client/src/components/misc/connect/ConnectModal.js
@@ -0,0 +1,181 @@
+import React from 'react'
+import {Modal,Button} from 'react-bootstrap'
+import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core'
+import { injected, network, walletconnect } from '../../../library/connector'
+import { useInactiveListener } from '../../../library/hooks'
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected
+} from '@web3-react/injected-connector'
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector'
+import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from '@web3-react/frame-connector'
+
+
+const ConnectorNames = {
+    Injected : 'MetaMask',
+    Network : 'Network',
+    WalletConnect : 'WalletConnect'
+  };
+  Object.freeze(ConnectorNames)
+  const connectorsByName = {
+    [ConnectorNames.Injected]: injected,
+    [ConnectorNames.Network]: network,
+    [ConnectorNames.WalletConnect]: walletconnect
+  }
+  
+  function getErrorMessage(error) {
+    if (error instanceof NoEthereumProviderError) {
+      return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+    } else if (error instanceof UnsupportedChainIdError) {
+      return "You're connected to an unsupported network."
+    } else if (
+      error instanceof UserRejectedRequestErrorInjected ||
+      error instanceof UserRejectedRequestErrorWalletConnect ||
+      error instanceof UserRejectedRequestErrorFrame
+    ) {
+      return 'Please authorize this website to access your Ethereum account.'
+    } else {
+      console.error(error)
+      return 'An unknown error occurred. Check the console for more details.'
+    }
+  }
+  
+
+
+
+function ConnectModal(props) {
+    const context = useWeb3React()
+    const { connector, library, chainId, account, activate, deactivate, active, error } = context
+  
+    const [activatingConnector, setActivatingConnector] = useState()
+    useEffect(() => {
+      if (activatingConnector && activatingConnector === connector) {
+        setActivatingConnector(undefined)
+      }
+    }, [activatingConnector, connector])
+  
+    // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
+    const triedEager = props.tried
+  
+    // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
+    useInactiveListener(!triedEager || !!activatingConnector)
+    return (
+        <Modal
+          show={props.open}
+          onHide={props.onClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{props.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+                    
+    {Object.keys(connectorsByName).map((name) => {
+          const currentConnector = connectorsByName[name]
+          const activating = currentConnector === activatingConnector
+          const connected = currentConnector === connector
+          const disabled = !triedEager || !!activatingConnector || connected || !!error
+
+          return (
+           <div className={classes.modal}>
+           {activating && (<LoadingSpinner/>)}
+           <div className={classes.modal_btn}>
+            <button
+              disabled={disabled}
+              key={name}
+              onClick={() => {
+                setActivatingConnector(currentConnector)
+                activate(connectorsByName[name])
+              }}
+            >
+
+{connected && (
+                  <span role="img" aria-label="check">
+                    ✅
+                  </span>
+                )}
+              {name}
+              {name==='MetaMask' && (<img src={meta} alt=""/>)}
+            {name==='WalletConnect' && (<img src={wltcnct} alt=""/>)}
+            {name==='Network' && (<img src={net} alt=""/>)}
+              
+            </button>
+    </div>
+           </div>
+           
+          )
+        })}
+      
+      <div className={classes['button--alt2']}>
+        {(active || error) && (
+          <button
+            onClick={() => {
+              deactivate()
+            }}
+          >
+            Deactivate
+          </button>
+        )}
+      </div>
+
+  
+      <div className={classes.modal_btn}>
+        {/* {!!(library && account) && (
+          <button
+            onClick={() => {
+              library
+                .getSigner(account)
+                .signMessage('👋')
+                .then((signature) => {
+                  window.alert(`Success!\n\n${signature}`)
+                })
+                .catch((error) => {
+                  window.alert('Failure!' + (error && error.message ? `\n\n${error.message}` : ''))
+                })
+            }}
+          >
+            Sign Message
+          </button>
+        )} */}
+        {!!(connector === connectorsByName[ConnectorNames.Network] && chainId) && (
+          <button
+            onClick={() => {
+              ;(connector).changeChainId(chainId === 1 ? 4 : 1)
+            }}
+          >
+            Switch Networks
+          </button>
+        )}
+        {connector === connectorsByName[ConnectorNames.WalletConnect] && (
+          <button
+          
+            onClick={() => {
+              ;(connector).close()
+            }}
+          >
+            Kill WalletConnect Session
+          </button>
+        )}
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={props.onClose}>
+              Close
+            </Button>
+            {/* <Button variant="primary">Understood</Button> */}
+          </Modal.Footer>
+        </Modal>
+    )
+}
+
+
+export default ConnectModal;
+
--- client/src/pages/leaderboard/Leaderboard.js
+++ client/src/pages/leaderboard/Leaderboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+ 
 
 function Leaderboard(props) {
     return (
