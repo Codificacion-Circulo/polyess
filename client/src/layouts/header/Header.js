fb49359cbf3289d0fc8d888dcbdadaf838acea80
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-23 05:30:07 +0530

    web3 logic completed with error management

--- client/src/layouts/header/Header.js
+++ client/src/layouts/header/Header.js
@@ -1,9 +1,9 @@
-import {Fragment,useState,useEffect} from "react"
+import { Fragment, useState, useEffect } from "react"
 import './Header.css'
 import logo from "../../logo.svg";
-import {Navbar,Container,Nav,Toast,ToastContainer} from 'react-bootstrap';
+import { Navbar, Container, Nav, Toast, ToastContainer } from 'react-bootstrap';
 import ConnectModal from '../../components/misc/connect/ConnectModal'
-import { useWeb3React, UnsupportedChainIdError} from '@web3-react/core'
+import { useWeb3React, UnsupportedChainIdError } from '@web3-react/core'
 import { useEagerConnect } from '../../library/hooks'
 import {
   NoEthereumProviderError,
@@ -27,14 +27,14 @@ const changeNetwork = async () => {
 function getErrorMessage(error) {
   if (error instanceof NoEthereumProviderError) {
     return {
-      title:"Metmask Not Found",
-      msg:'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+      title: "Metmask Not Found",
+      msg: 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
     }
   } else if (error instanceof UnsupportedChainIdError) {
     changeNetwork()
     return {
-      title:"Unsupported Network",
-      msg:"You're connected to an unsupported network."
+      title: "Unsupported Network",
+      msg: "You're connected to an unsupported network."
     }
   } else if (
     error instanceof UserRejectedRequestErrorInjected ||
@@ -42,21 +42,22 @@ function getErrorMessage(error) {
     error instanceof UserRejectedRequestErrorFrame
   ) {
     return {
-      title:"Wallet Connect Request Rejected",
-      msg:'Please authorize this website to access your Ethereum account.'
+      title: "Wallet Connect Request Rejected",
+      msg: 'Please authorize this website to access your Ethereum account.'
     }
   } else {
     console.error(error)
     return {
-      title:'Unknown Error',
-      msg:'An unknown error occurred. Check the console for more details.'}
+      title: 'Unknown Error',
+      msg: 'An unknown error occurred. Check the console for more details.'
+    }
   }
 }
 
 
 function Header(props) {
   const context = useWeb3React()
-    const { connector, library, chainId, account, activate, deactivate, active, error } = context
+  const { connector, library, chainId, account, activate, deactivate, active, error } = context
   const triedEager = useEagerConnect()
   const [modal, setModal] = useState(false);
   const closeModalHandler = (props) => {
@@ -65,22 +66,22 @@ function Header(props) {
   const openModalHandler = (props) => {
     setModal(true);
   }
-    return (
-      <Fragment>
-      {modal && (<ConnectModal onClose={closeModalHandler} open={modal} tried={triedEager}/>)}
-        <Navbar sticky="top" style={{backgroundColor:" #e3f2fd"}} expand="lg">
+  return (
+    <Fragment>
+      {modal && (<ConnectModal onClose={closeModalHandler} open={modal} tried={triedEager} />)}
+      <Navbar sticky="top" style={{ backgroundColor: " #e3f2fd" }} expand="lg">
         <Container>
-        <Navbar.Brand href="/home">
-        <img
-          alt=""
-          src={logo}
-          width="60"
-          height="60"
-          className="d-inline-block align-center"
-        />{' '}
-      polyess
-      </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="justify-content-center align-items-center text-center"/>
+          <Navbar.Brand href="/home">
+            <img
+              alt=""
+              src={logo}
+              width="60"
+              height="60"
+              className="d-inline-block align-center"
+            />{' '}
+            polyess
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="justify-content-center align-items-center text-center" />
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav className="justify-content-center">
               <Nav.Link className="hover-underline-animation mx-3 text-center text-dark" href="/">Home</Nav.Link>
@@ -90,30 +91,30 @@ function Header(props) {
               <Nav.Link className="hover-underline-animation mx-3 text-center text-dark" href="/profile">Profile</Nav.Link>
               <Nav.Link className="hover-underline-animation mx-3 text-center text-dark" href="/faq">Faq</Nav.Link>
               <button onClick={openModalHandler} className="btn btn-primary">{account === null
-          ? 'Network Connected'
-          : account
-          ? `${account.substring(0, 6)}...${account.substring(account.length - 4)}`
-          : 'Connect'}</button>
+                ? 'Network Connected'
+                : account
+                  ? `${account.substring(0, 6)}...${account.substring(account.length - 4)}`
+                  : 'Connect'}</button>
 
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {error&& 
-        <ToastContainer className="p-3" position= 'middle-end'>
-      <Toast show={true} onClose={()=>{deactivate();openModalHandler()}} className="d-inline-block m-1" bg='danger' key='Danger'>
-    <Toast.Header>
-      <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-      <strong className="me-auto">{getErrorMessage(error).title}</strong>
-      <small>Just Now</small>
-    </Toast.Header>
-    <Toast.Body>
-      {getErrorMessage(error).msg}
-    </Toast.Body>
-  </Toast>
-  </ToastContainer>}
-      </Fragment>
-    )
+      {error &&
+        <ToastContainer className="p-3" position='middle-end'>
+          <Toast show={true} onClose={() => { deactivate(); openModalHandler() }} className="d-inline-block m-1" bg='danger' key='Danger'>
+            <Toast.Header>
+              <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+              <strong className="me-auto">{getErrorMessage(error).title}</strong>
+              <small>Just Now</small>
+            </Toast.Header>
+            <Toast.Body>
+              {getErrorMessage(error).msg}
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>}
+    </Fragment>
+  )
 }
 
 
