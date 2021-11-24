e77f01af56c8b73ff5bff7382ce74b76995a14ba
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-24 18:30:13 +0530

    test

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const gameLogic = require('./game-logic')
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+io.on('connection', client => {
+    gameLogic.initializeGame(io, client)
+})
+server.listen(process.env.PORT || 8000)
