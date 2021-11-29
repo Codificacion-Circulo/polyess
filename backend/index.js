d84d56167de149ed2f275ee039c2165fee4aa647
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-11-29 06:21:22 +0530

    socket final changes done

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const http = require('http')
 const socketio = require('socket.io')
 const gameLogic = require('./game-logic')
 const app = express()
+
+router.get('/',async (_,res)=>{
+    try{
+        res.send(<h1>Wait what? How did u found me?</h1>)
+    }catch(e){
+        res.status(500).send()
+    }
+})
 const server = http.createServer(app)
 const io = socketio(server)
 io.on('connection', client => {
