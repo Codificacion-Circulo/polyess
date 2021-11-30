f2afbb618662d963741f0d6d67070262cb44ebdd
Ojas Sinha <sinhaojas67@gmail.com>
2021-11-30 22:59:44 +0530

    app: polyess
    Signed-off-by: Ojas Sinha <sinhaojas67@gmail.com>

---
+++ app/lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:polyess/models/style.dart';
+import 'package:polyess/screens/home.dart';
+
+class Polyess extends StatelessWidget {
+  const Polyess({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Polyess',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.interTextTheme().copyWith(
+          bodyText1: textStyle1,
+        )
+      ),
+      home: Home(),
+    );
+  }
+}
---
+++ app/lib/models/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const defaultFontSize = 15.0;
+
+const textStyle1 = TextStyle(
+  fontSize: defaultFontSize,
+  fontWeight: FontWeight.w400,
+  color: Colors.black
+);
