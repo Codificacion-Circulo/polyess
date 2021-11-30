c544e843ec6a7de505ed3e5d3f4fd3ebe4d8dca5
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-01 03:10:31 +0530

    conditional rendring with auto injected connect done

--- client/src/integration/onboard/joinroom.js
+++ client/src/integration/onboard/joinroom.js
@@ -1,65 +1,46 @@
-import React from 'react'
+import {Fragment,useState} from 'react'
+import { useWeb3React} from '@web3-react/core'
 import JoinGame from './joingame'
 import ChessGame from '../../components/game/chessUI/chessgame'
 
 
-/**
- * Onboard is where we create the game room.
- */
 
-class JoinRoom extends React.Component {
-    state = {
-        didGetUserName: false,
-        inputText: ""
-    }
+function JoinRoom(props) {
+     const context = useWeb3React()
+    const { connector, library, chainId, account, activate, deactivate, active, error } = context
+    const inputText=account
+    const [didGetUserName,setDidGetUserName]= useState(false);
+    return (
+        <Fragment>
+        {account?
+            didGetUserName ? 
+            <Fragment>
+                <JoinGame userName = {inputText} isCreator = {false}/>
+                <ChessGame myUserName = {inputText}/>
+            </Fragment>
+        :
+           <div>
+                <h1 style={{textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px"}}>Your Username:</h1>
 
-    constructor(props) {
-        super(props);
-        this.textArea = React.createRef();
-    }
-
-    typingUserName = () => {
-        // grab the input text from the field from the DOM 
-        const typedText = this.textArea.current.value
-        
-        // set the state with that text
-        this.setState({
-            inputText: typedText
-        })
-    }
-
-    render() {
-    
-        return (<React.Fragment>
-            {
-                this.state.didGetUserName ? 
-                <React.Fragment>
-                    <JoinGame userName = {this.state.inputText} isCreator = {false}/>
-                    <ChessGame myUserName = {this.state.inputText}/>
-                </React.Fragment>
-            :
-               <div>
-                    <h1 style={{textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px"}}>Your Username:</h1>
-
-                    <input style={{marginLeft: String((window.innerWidth / 2) - 120) + "px", width: "240px", marginTop: "62px"}} 
-                           ref = {this.textArea}
-                           onInput = {this.typingUserName}></input>
-                           
-                    <button className="btn btn-primary" 
-                        style = {{marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px"}} 
-                        disabled = {!(this.state.inputText.length > 0)} 
-                        onClick = {() => {
-                            // When the 'Submit' button gets pressed from the username screen,
-                            // We should send a request to the server to create a new room with
-                            // the uuid we generate here.
-                            this.setState({
-                                didGetUserName: true
-                            })
-                        }}>Submit</button>
-                </div>
-            }
-            </React.Fragment>)
-    }
+                <h4 style={{marginLeft: String((window.innerWidth / 2) - 120) + "px", width: "240px", marginTop: "62px"}} 
+                      >{inputText}</h4>
+                       
+                       <button className="btn btn-primary" 
+                    style = {{marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px"}} 
+                    disabled = {!account} 
+                    onClick = {() => {
+                        // When the 'Submit' button gets pressed from the username screen,
+                        // We should send a request to the server to create a new room with
+                        // the uuid we generate here.
+                        setDidGetUserName(true)
+                    }}>Submit</button>
+            </div>:<h1>Connect</h1>
+        }
+        </Fragment>
+    )
 }
 
+
+
 export default JoinRoom
+
