a0e2d5faf0650bcc2442824542bfd13bf216cbc6
Ojas Sinha <sinhaojas67@gmail.com>
2021-12-01 18:46:40 +0530

    app: introduce bottomnavbar

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:polyess/screens/home_screen.dart';
+import 'package:polyess/screens/leaderboard_screen.dart';
+import 'package:polyess/screens/market_screen.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  PageController pageController = PageController();
+  int _index = 0;
+
+  void onTapped(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final _pageController = PageController();
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.leaderboard),
+              label: 'Leader Board',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_basket_rounded),
+              label: 'Market Place',
+            ),
+          ],
+          selectedItemColor: Colors.blue,
+          currentIndex: _index,
+          onTap: onTapped,
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+        ),
+        body: PageView(
+          controller: _pageController,
+          children: [
+            HomeScreen(),
+            LeaderBoardSceen(),
+            MarketPlaceScreen(),
+          ],
+        ),
+      ),
     );
   }
 }
