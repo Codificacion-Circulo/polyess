025907a34bbe09f64740763e11f3cfef29dfa5f7
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-03 19:21:53 +0530

    misc

--- client/src/pages/market/Market.js
+++ client/src/pages/market/Market.js
@@ -1,5 +1,8 @@
 import React, { Fragment } from 'react'
 import './Market.css'
+import blue from '../../assets/market/blue.png'
+import red from '../../assets/market/red.png'
+import golden from '../../assets/market/golden.png'
 
 function Market(props) {
     return (
@@ -19,29 +22,41 @@ function Market(props) {
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-4">
-    <h4 class="text-center"><strong>STYLE 1</strong></h4>
-    <hr/>
-    <div class="profile-card-2"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-2.jpg" class="img img-responsive"/>
-        <div class="profile-name">JOHN DOE</div>
-        <div class="profile-username">@johndoesurname</div>
-        <div class="profile-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
-    </div>
-</div>
+		
 
 <div class="col-md-4">
-    <h4 class="text-center"><strong>STYLE 2</strong></h4>
-    <hr/>
-    <div class="profile-card-6"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-6.jpg" class="img img-responsive"/>
-        <div class="profile-name">JOHN
-            <br/>DOE</div>
-        <div class="profile-position">Lorem Ipsum Donor</div>
+    <div class="profile-card-6"><img src={blue} class="img img-responsive"/>
+        <div class="profile-name">VISHWANATH
+            <br/>AANAND</div>
         <div class="profile-overview">
             <div class="profile-overview">
                 <div class="row text-center">
                     <div class="col-xs-4">
-                        <h3>1</h3>
-                        <p>Rank</p>
+                        <h3 className="text-white">969</h3>
+                        <p className="text-danger">Rank</p>
+                    </div>
+                   
+                    <div class="col-xs-4">
+                    <h3 className="text-white">INDIA</h3>
+                        <p className="text-danger">COUNTRY</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-4">
+    <div class="profile-card-6"><img src={red} class="img img-responsive"/>
+        <div class="profile-name">JOHN
+            <br/>DOE</div>
+        <div class="profile-overview">
+            <div class="profile-overview">
+                <div class="row text-center">
+                    <div class="col-xs-4">
+                        <h3 className="text-light">969</h3>
+                        <p className="text-danger">Rank</p>
                     </div>
                     <div class="col-xs-4">
                         <h3>50</h3>
@@ -56,30 +71,28 @@ function Market(props) {
         </div>
     </div>
 </div>
-		<div class="col-md-4">
-    <h4 class="text-center"><strong>STYLE 3</strong></h4>
-    <hr/>
-    <div class="profile-card-4 text-center"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-4.jpg" class="img img-responsive"/>
-        <div class="profile-content">
-            <div class="profile-name">John Doe
-                <p>@johndoedesigner</p>
-            </div>
-            <div class="profile-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</div>
-            <div class="row">
-                <div class="col-xs-4">
-                    <div class="profile-overview">
-                        <p>TWEETS</p>
-                        <h4>1300</h4></div>
-                </div>
-                <div class="col-xs-4">
-                    <div class="profile-overview">
-                        <p>FOLLOWERS</p>
-                        <h4>250</h4></div>
-                </div>
-                <div class="col-xs-4">
-                    <div class="profile-overview">
-                        <p>FOLLOWING</p>
-                        <h4>168</h4></div>
+		
+
+
+<div class="col-md-4">
+    <div class="profile-card-6"><img src={golden} class="img img-responsive"/>
+        <div class="profile-name">JOHN
+            <br/>DOE</div>
+        <div class="profile-overview">
+            <div class="profile-overview">
+                <div class="row text-center">
+                    <div class="col-xs-4">
+                        <h3 className="text-light">969</h3>
+                        <p className="text-danger">Rank</p>
+                    </div>
+                    <div class="col-xs-4">
+                        <h3>50</h3>
+                        <p>Matches</p>
+                    </div>
+                    <div class="col-xs-4">
+                        <h3>35</h3>
+                        <p>Goals</p>
+                    </div>
                 </div>
             </div>
         </div>
