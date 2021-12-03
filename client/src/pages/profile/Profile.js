7cb77d862f70e6a7d40cefc67a6a821ebd03029b
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-03 18:16:46 +0530

    profile page handing over to sreyom

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -34,35 +34,35 @@ function Profile(props) {
     const { connector, library, chainId, account, activate, deactivate, active, error } = context
     const userAddr=account
     return (
-        <section class="section about-section gray-bg" id="about">
-            <div class="container">
+        <section class="section about-section" id="about">
+            <div class="container p-4 my-4 profile">
                 <div class="row align-items-center flex-row-reverse">
                     <div class="col-lg-6">
                         <div class="about-text go-to">
-                            <h3 class="dark-color">Deepak Kumar</h3>
+                            <h3 class="text-light">Deepak Kumar</h3>
                             <h6 class="theme-color lead">{userAddr}</h6>
-                            <p>specializing in creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through the bold interface and meaningful interactions.</p>
+                            <p className="text-light">specializing in creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through the bold interface and meaningful interactions.</p>
                             <div class="row about-list">
    
                                 <div class="col-md-6">
                                     <div class="media">
-                                        <label>E-mail</label>
-                                        <p>info@domain.com</p>
+                                        <label className="text-success">E-mail</label>
+                                        <p className="text-light">info@domain.com</p>
                                     </div>
                                     <div class="media">
-                                        <label>Twitter</label>
-                                        <p>820-885-3321</p>
+                                        <label className="text-success">Twitter</label>
+                                        <p className="text-light">820-885-3321</p>
                                     </div>
                             
                                 </div>
                                 <div class="col-md-6">
                                     <div class="media">
-                                        <label>Insta</label>
-                                        <p>4th april 1998</p>
+                                        <label className="text-success">Insta</label>
+                                        <p className="text-light">4th april 1998</p>
                                     </div>
                                     <div class="media">
-                                        <label>Joined</label>
-                                        <p>4th april 1998</p>
+                                        <label className="text-success">Joined</label>
+                                        <p className="text-light">4th april 1998</p>
                                     </div>
                             
                                 </div>
@@ -79,26 +79,26 @@ function Profile(props) {
                     <div class="row">
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="500" data-speed="500">500</h6>
-                                <p class="m-0px font-w-600">Tokens Count</p>
+                                <h6 class="count h2 text-success" data-to="500" data-speed="500">500</h6>
+                                <p class="m-0px font-w-600 text-light">Tokens Count</p>
                             </div>
                         </div>
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="150" data-speed="150">150</h6>
-                                <p class="m-0px font-w-600">NFT Count</p>
+                                <h6 class="count h2 text-success" data-to="150" data-speed="150">150</h6>
+                                <p class="m-0px font-w-600 text-light">NFT Count</p>
                             </div>
                         </div>
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="850" data-speed="850">850</h6>
-                                <p class="m-0px font-w-600">Games Won</p>
+                                <h6 class="count h2 text-success" data-to="850" data-speed="850">850</h6>
+                                <p class="m-0px font-w-600 text-light">Games Won</p>
                             </div>
                         </div>
                         <div class="col-6 col-lg-3">
                             <div class="count-data text-center">
-                                <h6 class="count h2" data-to="190" data-speed="190">190</h6>
-                                <p class="m-0px font-w-600">Total Games Played</p>
+                                <h6 class="count h2 text-success" data-to="190" data-speed="190">190</h6>
+                                <p class="m-0px font-w-600 text-light">Total Games Played</p>
                             </div>
                         </div>
                     </div>
