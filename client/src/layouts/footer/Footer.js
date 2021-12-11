591a9f08f4f4323a1e3df55d7cdb532ed815fa9f
sreyom31 <sreyom31@gmail.com>
2021-12-12 00:09:53 +0530

    footer icons
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- client/src/layouts/footer/Footer.js
+++ client/src/layouts/footer/Footer.js
@@ -10,9 +10,9 @@ function Footer() {
 		<div className="row align-items-center mx-3">
 
 			<div className="col-md-7 col-lg-8">
-				<p>	Copyend ©2020 All ends reserved by:
+				<p>	Copyend ©2021 All ends reserved by:
 					<a href="#" style={{ textDecoration: "none"}}>
-						<strong style={{color: "#ff1744"}} > The Providers</strong>
+						<strong style={{color: "#ff1744"}} > The Blitzkreig</strong>
 					</a></p>
 				
 			</div>
@@ -22,19 +22,19 @@ function Footer() {
 
                 <ul className="list-unstyled list-inline">
 						<li className="list-inline-item">
-							<a href="#" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i className="fab fa-facebook"></i></a>
+							<a href="/game" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i class="fas fa-gamepad"></i></a>
 						</li>
 						<li className="list-inline-item">
-							<a href="#" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i className="fab fa-twitter"></i></a>
+							<a href="/leaderboard" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i class="fas fa-chart-bar"></i></a>
 						</li>
 						<li className="list-inline-item">
-							<a href="#" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i className="fab fa-google-plus"></i></a>
+							<a href="/market" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i class="fas fa-shopping-cart"></i></a>
 						</li>
 						<li className="list-inline-item">
-							<a href="#" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i className="fab fa-linkedin-in"></i></a>
+							<a href="#" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i class="fab fa-app-store-ios"></i></a>
 						</li>
 						<li className="list-inline-item">
-							<a href="#" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i className="fab fa-youtube"></i></a>
+							<a href="/faq" className="btn-floating btn-sm text-white" style={{fontSize: "23px"}}><i class="fas fa-question-circle"></i></a>
 						</li>
 					</ul>
 					
