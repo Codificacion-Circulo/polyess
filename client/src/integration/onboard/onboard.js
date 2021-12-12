457b8016fd3294c8f4519659b21cf1758ea2ff95
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-12 21:47:14 +0530

    few changes

--- client/src/integration/onboard/onboard.js
+++ client/src/integration/onboard/onboard.js
@@ -1,5 +1,6 @@
-import {useState,useRef,Fragment,useContext} from 'react'
-import { Redirect } from 'react-router-dom'
+import {useState,useEffect,Fragment,useContext} from 'react'
+import axios from 'axios';
+import { Redirect,useParams } from 'react-router-dom'
 import uuid from 'uuid/v4'
 import { ColorContext } from '../../store/colorcontext' 
 import { useWeb3React} from '@web3-react/core'
@@ -8,17 +9,18 @@ const socket  = require('../connection/socket').socket
 
 function CreateNewGame(props) {
     const context = useWeb3React()
+    const params=useParams();
     const { connector, library, chainId, account, activate, deactivate, active, error } = context
     const inputText=account
     const [didGetUserName,setDidGetUserName]= useState(false);
     const [gameId,setGameId]=useState("");
     const [userData,setUserData] = useState([]);
     var data = JSON.stringify({
-        "address": account
+        "address": account||'0x596F08aDAa76889161A98c9Bb79869e7f9518C70'
       });
       var config = {
         method: 'post',
-        url: 'http://polyess-listner.herokuapp.com/search',
+        url: 'http://polyess-listner.herokuapp.com/login',
         headers: { 
           'Content-Type': 'application/json'
         },
@@ -44,7 +46,12 @@ function CreateNewGame(props) {
         setGameId(newGameRoomId)
         socket.emit('createNewGame', newGameRoomId)
     }
-
+    const optionArray=[]
+    for (const nft in userData.nfts) {
+      console.log(nft)
+      const arr=(<option value={nft.name}>{nft.name}</option>)
+        optionArray.push(arr)
+    }
     return (
         <Fragment>
         {account?
@@ -54,11 +61,27 @@ function CreateNewGame(props) {
 
         :
            <div>
-                <h1 style={{textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px"}} className="text-white">Your Username: {inputText} </h1>
+                <h1 style={{textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px"}} className="text-danger">Your Username: {userData&&userData.username} </h1>
                 <h1 style={{textAlign: "center", marginTop: String((window.innerHeight / 8)) + "px"}} className="text-white mb-4">Your Address: {`${account.substring(0, 6)}..${account.substring(account.length-4)}`} </h1>
                 {/* <h4 style={{marginLeft: String((window.innerWidth / 2) - 120) + "px", width: "240px", marginTop: "62px"}} 
                       >{inputText}</h4> */}
-                       
+                {params.id==='1'&&
+                      <div className="text-center">
+                      <h4 className="text-warning">Amount of Token to Stake</h4>
+                  <input type="number" />
+                      </div>
+                }
+                {params.id==='2'&&
+                      <div className="text-center">
+                      <h4 className="text-warning">NFT id to Stake</h4>
+                 
+<select id="cars" name="cars">
+  {/* <option value="volvo">Volvo XC90</option> */}
+  {optionArray}
+  
+</select>
+                      </div>
+                }
                 <button className="btn btn-primary mb-4" 
                     style = {{marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px"}} 
                     disabled = {!account} 
