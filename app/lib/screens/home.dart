10fa50410ace7d34b2b42042f7a2bc7e2850e5e1
Ojas Sinha <sinhaojas67@gmail.com>
2021-12-12 07:05:24 +0530

    app: make pageview unscrollable
    Signed-off-by: Ojas Sinha <sinhaojas67@gmail.com>

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -60,6 +60,7 @@ class _HomeState extends State<Home> {
             LeaderBoardSceen(),
             MarketPlaceScreen(),
           ],
+          physics: NeverScrollableScrollPhysics(),
         ),
       ),
     );
