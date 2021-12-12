5db7c0d40a6fca9538af895c7ff42eddd50332c5
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2021-12-12 21:18:23 +0530

    marketplace changes

--- app/lib/screens/market_screen.dart
+++ app/lib/screens/market_screen.dart
@@ -29,81 +29,97 @@ class _MarketPlaceScreenState extends State<MarketPlaceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text('Polyess'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  WalletService().clearData();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => WalletConnect(),
+                    ),
+                  );
+                },
+                icon: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ))
+          ],
+        ),
         body: GridView.count(
-      crossAxisCount: 2,
-      children: List.generate(3, (index) {
-        return GestureDetector(
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.amberAccent[100],
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-              margin: EdgeInsets.all(10.0),
-              child: Image.network(
-                  "https://gateway.pinata.cloud/ipfs/QmPWCagNgzp5P2TigD471JMr2bzjkhsjLEQFHTR4hAqnrg/1.png")),
-          onTap: () {
-            _launchURL();
-            // log("${await NFTApi().fetchNTFOwner()}");
-            // log("${await NFTApi().validBuy("61b5d2326f91e091b233e4e7")}");
-            // if (await NFTApi().validBuy("61b5d2326f91e091b233e4e7") ==
-            //     "cannot buy") {
-            //   log("message");
-            // } else {
-            //   log("message2");
-            // }
-            // showModalBottomSheet(
-            //   backgroundColor: bgColor,
-            //   context: context,
-            //   builder: (BuildContext context) {
-            //     return Padding(
-            //         padding: const EdgeInsets.all(15.0),
-            //         child: NFTApi().validBuy("61b5d2a26f91e091b233e4ea") ==
-            //                 "canbuy"
-            //             ? Padding(
-            //                 padding: const EdgeInsets.all(15.0),
-            //                 child: Row(
-            //                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //                   children: [
-            //                     ElevatedButton(
-            //                       style: ElevatedButton.styleFrom(
-            //                           shape: RoundedRectangleBorder(
-            //                               borderRadius:
-            //                                   BorderRadius.circular(15)),
-            //                           minimumSize: Size(75, 75)),
-            //                       onPressed: () {},
-            //                       child: Text(
-            //                         'Buy',
-            //                         style: textStyle1,
-            //                       ),
-            //                     ),
-            //                     ElevatedButton(
-            //                       style: ElevatedButton.styleFrom(
-            //                           shape: RoundedRectangleBorder(
-            //                               borderRadius:
-            //                                   BorderRadius.circular(15)),
-            //                           minimumSize: Size(75, 75)),
-            //                       onPressed: () {},
-            //                       child: Text('Sell', style: textStyle1),
-            //                     ),
-            //                   ],
-            //                 ),
-            //               )
-            //             : ElevatedButton(
-            //                 style: ElevatedButton.styleFrom(
-            //                     shape: RoundedRectangleBorder(
-            //                         borderRadius: BorderRadius.circular(15)),
-            //                     minimumSize: Size(75, 75)),
-            //                 onPressed: null,
-            //                 child: Text('Owned By Someone', style: textStyle1),
-            //               ));
-            //   },
-            // );
-            // setState(() {
-            //   context;
-            // });
-          },
-        );
-      }),
-    ));
+          crossAxisCount: 2,
+          children: List.generate(3, (index) {
+            return GestureDetector(
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.amberAccent[100],
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  margin: EdgeInsets.all(10.0),
+                  child: Image.network(
+                      "https://gateway.pinata.cloud/ipfs/QmPWCagNgzp5P2TigD471JMr2bzjkhsjLEQFHTR4hAqnrg/1.png")),
+              onTap: () async {
+                // _launchURL();
+                // log("${await NFTApi().validBuy("61b5d2326f91e091b233e4e7")}");
+                if (await NFTApi().validBuy("61b5d2326f91e091b233e4e7")) {
+                  log("message");
+                } else {
+                  log("message2");
+                }
+                showModalBottomSheet(
+                  backgroundColor: bgColor,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: NFTApi().validBuy("61b5d2a26f91e091b233e4ea")
+                            ? Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(15)),
+                                          minimumSize: Size(75, 75)),
+                                      onPressed: () {},
+                                      child: Text(
+                                        'Buy',
+                                        style: textStyle1,
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(15)),
+                                          minimumSize: Size(75, 75)),
+                                      onPressed: () {},
+                                      child: Text('Sell', style: textStyle1),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                    minimumSize: Size(75, 75)),
+                                onPressed: null,
+                                child:
+                                    Text('Owned By Someone', style: textStyle1),
+                              ));
+                  },
+                );
+              },
+            );
+          }),
+        ));
   }
 }
 
