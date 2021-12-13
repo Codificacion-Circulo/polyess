4a559df3fcce5f0b49717a8ab669bb3211cd634a
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-13 20:32:42 +0530

    onboard join game checked

--- client/src/components/game/onboard/joingame.js
+++ client/src/components/game/onboard/joingame.js
@@ -1,22 +1,17 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-const socket = require('../../../integration/connection/socket').socket
-
+import React from 'react';
+import { useParams } from 'react-router-dom';
+const socket = require('../../../integration/connection/socket').socket;
 const JoinGameRoom = (gameid, userName, isCreator) => {
     const idData = {
         gameId: gameid,
         userName: userName,
         isCreator: isCreator
     }
-    socket.emit("playerJoinGame", idData)
-}
-
-
+    socket.emit("playerJoinGame", idData);
+};
 const JoinGame = (props) => {
-    const { gameid } = useParams()
-    JoinGameRoom(gameid, props.userName, props.isCreator)
-    return <div>
-    </div>
-}
-
-export default JoinGame
+    const { gameid } = useParams();
+    JoinGameRoom(gameid, props.userName, props.isCreator);
+    return <div></div>
+};
+export default JoinGame;
