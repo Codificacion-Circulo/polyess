7767bfaa892fb69c4ba352a5f7705e161d184e29
sreyom31 <sreyom31@gmail.com>
2021-12-14 02:08:33 +0530

    faqs added

--- client/src/components/faqs/Faqs.js
+++ client/src/components/faqs/Faqs.js
@@ -13,8 +13,7 @@ const data = {
       content: `There are 2 ways to get hess tokens. One you buy the tokens in exchange of ethers or you win from your friends. Each Token costs 10^-12 Ether. And you need at least 10^8 HT tokens to start staking.`,
     },
     {
-      title:
-        "What are NFTs? I don't understand all this rocket science GenZ is talking about?",
+      title: "What are NFTs? I don't understand all this rocket science GenZ is talking about?",
       content: `Non-Fungible tokens of NFTs means they are unique and can’t be traded for other NFTs. Like one bitcoin can be exchanged for another bitcoin, NFTs are not like that. Their uniqueness is backed by technology like blockchain and to be honest anything digital can be NFT, picture, mp3 files, books, anything you can imagine. We created NFTs of famous chess players, if you like art, buy it at the marketplace.`,
     },
     {
@@ -34,13 +33,24 @@ const data = {
       content: `Go to the profile section, Each profile has a user name and wallet ID which is connected. It provides a detail view of Tokens, NFTs, games won and games lost. And also displays what NFTs you have with game history. `,
     },
     {
-      title:
-        "I am new to blockchain. Is there any knowledge that I need to have before starting?",
-      content: `You just should know how to use your Metamask Wallet as you will be playing with very much real money and also crypto is a ground for multiple scams so i would like you to be aware about that. You can check how to use metamask here.`,
+      title: "I am new to blockchain. Is there any knowledge that I need to have before starting?",
+      content: `You just should know how to use your Metamask Wallet as you will be playing with very much real money and also crypto is a ground for multiple scams so i would like you to be aware about that.`,
     },
     {
       title: "How does the leaderboard metrics work?",
       content: `For each win you get 2 points for each loss you get 1 point and for each draw you get 1.5 points. As we want to promote users to play chess more and more rather than their  The more you play the higher your rank is.`,
+    },
+    {
+      title: "What to do if you get an error in HT token staking?",
+      content: `First check if you and your friend are staking the right amount of tokens, then check if you both are staking more than limit? This should get the problem fixed. If not write to us at support@hesstoken.in.`,
+    },
+    {
+      title: "Is there an App for this? If yes, what features does it include?",
+      content: `Yes there is an app and it is a marketplace for ntfs and shows you your profile status with the comfort of your phone. We are working with the Google team to bring the app to the playstore so you should expect it as a feature coming soon.`,
+    },
+    {
+      title: "How are we decentralised?",
+      content: `All the transactions either be it NFTs, HT tokens or ether exchanges happen through the ethereum blockchain which is one of the most decentralised and distributed blockchain and thus we as owners does not have any control over your assets at all. `,
     },
   ],
 };
@@ -62,8 +72,8 @@ const config = {
 const Faqs = (props) => {
   return (
     <div>
-      <div class="logo my-3 text-center">
-        <h1 className="text-light">
+      <div class="logo my-3 text-center" >
+        <h1 className="text-center" style={{color: "#d1996d"}}>
           <b>Faqs</b>
         </h1>
       </div>
