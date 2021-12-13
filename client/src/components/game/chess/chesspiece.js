5d826fbc2f08c449ce3aff16e08ee6cf5ed8a7f9
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-13 19:04:39 +0530

    game/chess folder done

--- client/src/components/game/chess/chesspiece.js
+++ client/src/components/game/chess/chesspiece.js
@@ -1,38 +1,29 @@
 class ChessPiece {
     constructor(name, isAttacked, color, id) {
-        this.name = name // string
-        this.isAttacked = isAttacked // boolean
-        this.color = color // string
-        this.id = id // string
+        this.name = name ;
+        this.isAttacked = isAttacked;
+        this.color = color;
+        this.id = id ;
     }
 
     setSquare(newSquare) {
-        // set the square this piece is sitting top of. 
-        // on any given piece (on the board), there will always be a piece on top of it. 
-        // console.log(newSquare)
         if (newSquare === undefined) {
-            this.squareThisPieceIsOn = newSquare
-            return 
+            this.squareThisPieceIsOn = newSquare;
+            return ;
         }
-
         if (this.squareThisPieceIsOn === undefined) {
-            this.squareThisPieceIsOn = newSquare
-            newSquare.setPiece(this)
+            this.squareThisPieceIsOn = newSquare;
+            newSquare.setPiece(this);
         }
-
-        const isNewSquareDifferent = this.squareThisPieceIsOn.x != newSquare.x || this.squareThisPieceIsOn.y != newSquare.y
-
+        const isNewSquareDifferent = this.squareThisPieceIsOn.x != newSquare.x || this.squareThisPieceIsOn.y != newSquare.y;
         if (isNewSquareDifferent) {
-            // console.log("set")
-            this.squareThisPieceIsOn = newSquare
-            newSquare.setPiece(this)
+            this.squareThisPieceIsOn = newSquare;
+            newSquare.setPiece(this);
         }
     }
-
     getSquare() {
-        return this.squareThisPieceIsOn
+        return this.squareThisPieceIsOn;
     }
 }
 
-
-export default ChessPiece
+export default ChessPiece;
--- client/src/components/game/chess/square.js
+++ client/src/components/game/chess/square.js
@@ -1,58 +1,54 @@
 class Square {
     constructor(x, y, pieceOnThisSquare, canvasCoord) {
-        this.x = x // Int 0 < x < 7
-        this.y = y // Int 0 < y < 7 
-        this.canvasCoord = canvasCoord
-        this.pieceOnThisSquare = pieceOnThisSquare // ChessPiece || null
+        this.x = x;
+        this.canvasCoord = canvasCoord;
+        this.pieceOnThisSquare = pieceOnThisSquare;
     }
 
     setPiece(newPiece) {
         if (newPiece === null && this.pieceOnThisSquare === null) {
-            return
+            return;
         } else if (newPiece === null) {
-            // case where the function caller wants to remove the piece that is on this square. 
-            this.pieceOnThisSquare.setSquare(undefined)
-            this.pieceOnThisSquare = null
+            this.pieceOnThisSquare.setSquare(undefined);
+            this.pieceOnThisSquare = null;
         } else if (this.pieceOnThisSquare === null) {
-            // case where the function caller wants assign a new piece on this square
-            this.pieceOnThisSquare = newPiece
-            newPiece.setSquare(this)
+            this.pieceOnThisSquare = newPiece;
+            newPiece.setSquare(this);
         } else if (this.getPieceIdOnThisSquare() != newPiece.id && this.pieceOnThisSquare.color != newPiece.color) {
-            // case where the function caller wants to change the piece on this square. (only different color allowed)
-            console.log("capture!")
-            this.pieceOnThisSquare = newPiece
-            newPiece.setSquare(this)
+            console.log("capture!");
+            this.pieceOnThisSquare = newPiece;
+            newPiece.setSquare(this);
         } else {
-            return "user tried to capture their own piece"
+            return "user tried to capture their own piece";
         }
     }
 
     removePiece() {
-        this.pieceOnThisSquare = null
+        this.pieceOnThisSquare = null;
     }
 
     getPiece() {
-        return this.pieceOnThisSquare 
+        return this.pieceOnThisSquare ;
     }
 
     getPieceIdOnThisSquare() {
         if (this.pieceOnThisSquare === null) {
-            return "empty"
+            return "empty";
         }
-        return this.pieceOnThisSquare.id
+        return this.pieceOnThisSquare.id;
     }
 
     isOccupied() {
-        return this.pieceOnThisSquare != null
+        return this.pieceOnThisSquare != null;
     }
 
     getCoord() {
-        return [this.x, this.y]
+        return [this.x, this.y];
     }
 
     getCanvasCoord() {
-        return this.canvasCoord
+        return this.canvasCoord;
     }
 }
 
-export default Square
+export default Square;
