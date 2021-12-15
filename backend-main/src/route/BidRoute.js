c8cab7293a82b8552e77fdae22fcd51c7788b775
aptgetinfo <deepak.2002.02.16@gmail.com>
2021-12-15 20:35:58 +0530

    post req

--- backend-main/src/route/BidRoute.js
+++ backend-main/src/route/BidRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getAllBids, postBidCreated, postBidEnd } = require("../controllers/bidController")
-router.route("/bids").get(getAllBids);
+router.route("/bids").post(getAllBids);
 router.route("/bidCreated").post(postBidCreated);
 router.route("/bidEnd").post(postBidEnd);
 
